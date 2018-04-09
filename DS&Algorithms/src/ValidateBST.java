5bdeaed6cd8960a86bbbd3ba49bc14d482cc7f30
Shruthi <shruthi@Admins-MacBook-Pro.local>
2018-04-08 17:47:28 -0700

    Added comments

--- DS&Algorithms/src/ValidateBST.java
+++ DS&Algorithms/src/ValidateBST.java
@@ -1,4 +1,27 @@
-
+/***
+ * 
+ * 
+ * @author shruthi
+ * 
+ * 
+ * 
+ *         A BST is based on binary tree, but with the following additional
+ *         properties: - The left subtree of a node contains only nodes with
+ *         keys less than the node’s key. - The right subtree of a node contains
+ *         only nodes with keys greater than the node’s key. - Both the left and
+ *         right subtrees must also be binary search trees.
+ * 
+ *         O(n) runtime, O(n) stack space – Top-down recursion:
+ * 
+ *         This algorithm runs in O(n) time, where n is the number of nodes of
+ *         the binary tree. Sharp readers may notice that the above code does
+ *         not work if the tree contains the smallest or the largest integer
+ *         value. How could we fix this? One way to fix this is to use null to
+ *         represent the infinity.
+ * 
+ * 
+ *
+ */
 public class ValidateBST {
 	
 	public static void main(String args[])
